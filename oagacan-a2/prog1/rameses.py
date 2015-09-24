bc877660205031f59b8b01d01be86c7d8b956279
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-09-24 10:44:21 -0400

    init

---
+++ oagacan-a2/prog1/rameses.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    import argparse
+
+    arg_parser = argparse.ArgumentParser("Rameses player")
+    arg_parser.add_argument("board-size", type=int, nargs=1)
+    arg_parser.add_argument("board", type=str, nargs=1)
+    arg_parser.add_argument("time-limit", type=float, nargs=1)
+
+    args = vars(arg_parser.parse_args())
+    print args
