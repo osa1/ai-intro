6dc5ca310de7f25ba92237d312af3f6c37ccbcea
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-09-24 11:46:58 -0400

    implementing helpers(needs testing)

--- oagacan-a2/prog1/rameses.py
+++ oagacan-a2/prog1/rameses.py
@@ -10,8 +10,61 @@ class Grid:
         self.size = size
         self.grid = [c for c in str]
 
-    def at(self, row, col):
+    def at_xy(self, col, row):
         return self.grid[row * self.size + col]
+
+    def available_space(self):
+        for row in xrange(self.size):
+            for col in xrange(self.size):
+                if self.at_xy(col, row) == '.':
+                    yield (col, row)
+
+    def check_move_xy(self, col, row):
+        return self.__check_col(col, row) and \
+                self.__check_row(col, row) and \
+                self.__check_diagonal_1(col, row) and \
+                self.__check_diagonal_2(col, row)
+
+    # True  -> it's OK
+    # False -> avoid
+    def __check_col(self, col, row):
+        for x in range(self.size):
+            if x != col and self.at_xy(x, row) == '.':
+                return True
+        return False
+
+    # True  -> it's OK
+    # False -> avoid
+    def __check_row(self, col, row):
+        for y in range(self.size):
+            if y != row and self.at_xy(col, y) == '.':
+                return True
+        return False
+
+    def __check_diagonal_1(self, row, col):
+        "From top-left to bottom-right. True -> OK, False -> avoid."
+        if row != col:
+            return True
+
+        for xy in range(self.size):
+            if xy == row:
+                continue
+
+            if self.at_xy(xy, xy) == '.':
+                return True
+
+        return False
+
+    def __check_diagonal_2(self, row, col):
+        "From top-right to bottom-left. True -> OK, False -> avoid."
+        if row != self.size - 1 - col:
+            return True
+
+        for xy in range(self.size):
+            if self.at_xy(self.size - 1 - xy, xy) == '.':
+                return True
+
+        return False
 
     def __str__(self):
         lines = []
@@ -22,7 +75,7 @@ class Grid:
         for row in range(self.size):
             line = "| "
             for col in range(self.size):
-                line += self.at(row, col) + " | "
+                line += self.at_xy(col, row) + " | "
             lines.append(line)
             lines.append(line_sep)
 
@@ -38,4 +91,7 @@ if __name__ == "__main__":
 
     args = vars(arg_parser.parse_args())
     print args
-    print Grid(args["board-size"][0], args["board"][0])
+    grid = Grid(args["board-size"][0], args["board"][0])
+    print grid
+    print list(grid.available_space())
+    print grid.check_move_xy(2, 2)
